30d7a488c1355c8503408021e3673afd29af2bff
BDuman <bduman165@gmail.com>
2017-04-24 10:44:44 +0300

    örnek fortran 77 kodu

---
+++ fortran.for
@@ -0,0 +1,26 @@
+c yorum comment
+
+
+PROGRAM MAIN
+c asdsa dqw eqwe 23123 1231 23123
+    INTEGER N, X
+    EXTERNAL SUB1
+    COMMON /GLOBALS/ N
+    X = 0
+    PRINT *, 'Enter number of repeats'
+    READ (*,*) N
+    CALL SUB1(X,SUB1)
+    END
+
+    SUBROUTINE SUB1(X,DUMSUB)
+    INTEGER N, X, Y
+    EXTERNAL DUMSUB
+    COMMON /GLOBALS/ N
+    DATA Y /0/
+    IF(X .LT. N)THEN
+    X = X + 1
+    Y = Y + 1
+    PRINT *, 'x = ', X, ', y = ', Y
+    CALL DUMSUB(X,DUMSUB)
+    END IF
+END
